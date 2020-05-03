356a53105e0704fec7dce94ef0c0bbff5477f547
duhui <1052417164@qq.com>
2020-05-04 00:42:40 +0800

    看

--- spring cloud/springcloud_ribbon/src/main/java/springcloud_ribbon/TestController.java
+++ spring cloud/springcloud_ribbon/src/main/java/springcloud_ribbon/TestController.java
@@ -1,14 +1,19 @@
 package springcloud_ribbon;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 @Configuration
+@EnableHystrix
 public class TestController {
     @Bean
     @LoadBalanced
@@ -21,5 +26,20 @@ public class TestController {
         RestTemplate rt = getRestTemplate();
         String result = rt.getForObject("http://apiFirst/", String.class);
         return result;
+    }
+
+    /**
+     * 调用 user微服务
+     */
+    @HystrixCommand(fallbackMethod = "getDefaultUser")
+    @GetMapping("getUser")
+    public String getUser(Integer id) {
+        String url = "http://apiFirst/";
+        return getRestTemplate().getForObject(url, String.class);
+    }
+
+    public String getDefaultUser(Integer id) {
+        System.out.println("熔断，默认回调函数");
+        return "{\"id\":-1,\"name\":\"熔断用户\",\"password\":\"123456\"}";
     }
 }
