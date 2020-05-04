a78a11d791775ded225c2976de955d5614f174be
duhui <1052417164@qq.com>
2020-05-05 04:01:05 +0800

    asss

---
+++ spring cloud/springcloud_ribbon2/src/main/java/ribbonSecond/RibbonSecondApplication.java
@@ -0,0 +1,41 @@
+package ribbonSecond;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableHystrix
+public class RibbonSecondApplication {
+    @RequestMapping("/")
+    public String home(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String userAgent = request.getHeader("User-Agent");
+        String formFileName = "name";
+
+        // 针对IE或者以IE为内核的浏览器：
+        if (userAgent.contains("MSIE") || userAgent.contains("Trident")) {
+            formFileName = java.net.URLEncoder.encode(formFileName, "UTF-8");
+        } else {
+            // 非IE浏览器的处理：
+            formFileName = new String(formFileName.getBytes("UTF-8"), "ISO-8859-1");
+        }
+        response.setHeader("Content-disposition", String.format("attachment; filename=\"%s\"", formFileName));
+        response.setContentType("multipart/form-data");
+        response.setCharacterEncoding("UTF-8");
+        return "Hello world";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonSecondApplication.class, args);
+    }
+}
